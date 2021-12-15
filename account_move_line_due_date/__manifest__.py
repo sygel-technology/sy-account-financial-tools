afe654a0322aa433e1264fe7b89886e8c1decd91
manu <manuel.regidor@sygel.es>
2021-12-15 11:18:56 +0100

    T-2263[ADD]account_move_line_due_date, nuevo modulo

---
+++ account_move_line_due_date/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Move Line Due Date",
+    "summary": "Show Date Maturity on Journal Entries Lines",
+    "version": "13.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.sygel.es",
+    "author": "Sygel",
+    "license": "AGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+    ],
+}
