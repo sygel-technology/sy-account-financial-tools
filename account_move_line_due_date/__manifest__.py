9e5a59d28f43aa8209ad967d8efb7494bcd87d62
sygel <alberto.martinez@sygel.es>
2025-11-12 17:25:14 +0100

    [T-9076][MIG] account_move_line_due_date: Migration to 17.0

--- account_move_line_due_date/__manifest__.py
+++ account_move_line_due_date/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Account Move Line Due Date",
     "summary": "Shows Date Maturity on Journal Entries Lines",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Accounting",
     "website": "https://github.com/sygel-technology/sy-account-financial-tools",
     "author": "Sygel",
